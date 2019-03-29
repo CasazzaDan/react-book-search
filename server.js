c2db87ea710f9e95f0e87c1845f0d4fb4e5308e7
DCas973 <dan.casazza@endeveran.com>
2019-03-28 22:03:43 -0500

    troubleshootting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
